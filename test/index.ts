d358d44b8e1f3371e82d2f66ac443292d806432c
rahultripathi <rahutripathi@ewar.in>
2022-01-15 06:21:26 +0530

    added tests

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,125 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import web3 from "web3";
+import {
+  ArcadeLand,
+  ArcadeLand__factory,
+  Land,
+  Land__factory,
+} from "../typechain";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("ArcadeLand", function () {
+  let owner: SignerWithAddress,
+    renter: SignerWithAddress,
+    randomAddr: SignerWithAddress;
+  let ArcadeLandFactory: ArcadeLand__factory;
+  let landMinter: ArcadeLand;
+  let RentedLandsFactory: Land__factory;
+  let land: Land;
+  before(async () => {
+    ArcadeLandFactory = await ethers.getContractFactory("ArcadeLand");
+    [owner, renter, randomAddr] = await ethers.getSigners();
+    landMinter = await ArcadeLandFactory.deploy(
+      web3.utils.toBN(10).toString(),
+      web3.utils.toWei("0.001", "ether"),
+      web3.utils.toWei("0.001", "ether")
+    );
+    await landMinter.deployed();
+  });
+  it("should get appropriate values of default states", async function () {
+    expect(await landMinter.NAME()).to.equal("arcland");
+    expect(await landMinter.SYMBOL()).to.equal("ARC");
+  });
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  it("Should mint land to owner and check deployed land contract", async function () {
+    console.log(`miniting for user ${owner.address}`);
+    const mintTxn = await landMinter.connect(owner).mint("https://google.com", {
+      value: web3.utils.toWei("0.001", "ether"),
+    });
+    await mintTxn.wait();
+    const bal = await landMinter.balanceOf(owner.address);
+    expect(bal).to.eq(web3.utils.toBN(1).toString());
+    const NFTOwner = await landMinter.ownerOf(1);
+    expect(NFTOwner).to.eq(owner.address);
+    console.log(await landMinter.tokenURI(1));
+    const contractAddr = await landMinter.lands(1);
+    expect(contractAddr).to.not.eq(
+      "0x0000000000000000000000000000000000000000"
+    );
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    RentedLandsFactory = await ethers.getContractFactory("Land");
+    land = await RentedLandsFactory.attach(contractAddr);
+    expect(await land.getRentedLands()).to.length(0);
+    expect(await land.pendingProposal()).to.eq(web3.utils.toBN(0).toString());
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  it("Should add a proposal to the land contract", async function () {
+    const newProposalTxn = await land
+      .connect(renter)
+      .newProposal(
+        web3.utils.toBN(40).toString(),
+        web3.utils.toBN(40).toString(),
+        web3.utils.toBN(4).toString(),
+        web3.utils.toBN(4).toString(),
+        web3.utils.toBN(30).toString(),
+        web3.utils.toBN(1223).toString(),
+        randomAddr.address,
+        {
+          value: web3.utils.toWei("0.001", "ether"),
+        }
+      );
+    await newProposalTxn.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const pending = await land.pendingProposal();
+    expect(pending).to.eq(web3.utils.toBN(1).toString());
+    const pendingProposal = await land._rents(pending);
+    //  console.log(pendingProposal);
+  });
+
+  it("approve the current proposal", async function () {
+    const newApprovalTxn = await land.connect(owner).updateProposalStatus(true);
+    await newApprovalTxn.wait();
+    expect(await land.pendingProposal()).equal(web3.utils.toBN(0).toString());
+    const rentedLand = await land.getRentedLands();
+    expect(await land._tileOwners(web3.utils.toBN(4040).toString())).equal(
+      web3.utils.toBN(1).toString()
+    );
+  });
+  it("Should conflict existing assetMap", async () => {
+    await expect(
+      land
+        .connect(renter)
+        .newProposal(
+          web3.utils.toBN(41).toString(),
+          web3.utils.toBN(42).toString(),
+          web3.utils.toBN(4).toString(),
+          web3.utils.toBN(4).toString(),
+          web3.utils.toBN(30).toString(),
+          web3.utils.toBN(1223).toString(),
+          randomAddr.address,
+          {
+            value: web3.utils.toWei("0.001", "ether"),
+          }
+        )
+    ).to.be.revertedWith("proposal collision detected");
+  });
+  it("Should not conflict existing assetMap", async () => {
+    await expect(
+      land
+        .connect(renter)
+        .newProposal(
+          web3.utils.toBN(55).toString(),
+          web3.utils.toBN(55).toString(),
+          web3.utils.toBN(4).toString(),
+          web3.utils.toBN(4).toString(),
+          web3.utils.toBN(30).toString(),
+          web3.utils.toBN(1223).toString(),
+          randomAddr.address,
+          {
+            value: web3.utils.toWei("0.001", "ether"),
+          }
+        )
+    ).to.not.be.revertedWith("proposal collision detected");
   });
 });
